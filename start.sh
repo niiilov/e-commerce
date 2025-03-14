1bd9672312b963f3d3ad944c5f35ddd080b6a77b
niiilov <nilovaslanbek2006@gmail.com>
2025-03-14 18:12:54 +0300

    start

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
-cd cmd
-go run main.go
+#!/bin/bash
+
+APP_SRC="./cmd/main.go"
+
+if [ ! -f "$APP_SRC" ]; then
+  echo "Error: source file $APP_SRC not exists!"
+  exit 1
+fi
+
+echo "Compiling..."
+go run "$APP_SRC"
