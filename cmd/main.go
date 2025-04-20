fe1569b9afd36e86160882a393b789950abefe74
niiilov <nilovaslanbek2006@gmail.com>
2025-04-20 14:30:40 +0300

    g

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/niiilov/e-commerce/internal/application"
+import (
+	"github.com/niiilov/e-commerce/internal/application"
+	"github.com/niiilov/e-commerce/internal/db"
+)
 
 func main() {
+	db.InitDB()
 	api := application.New()
 	api.Routing()
 	api.Run()
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,27 @@
 package models
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	Id       int    `json: 'user_id'`
 	Username string `json: 'username'`
 	Email    string `json: 'email'`
 	Password string `json: 'pass'`
+}
+
+func (u *User) HashPassword() error {
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashPassword)
+
+	return nil
+}
+
+func (u *User) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 }
